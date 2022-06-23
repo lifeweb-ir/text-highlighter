60f4bf7bb8dd21ab1457e71c4e15fa388d21a38b
Javad Shariati <jsh1400@yahoo.com>
2022-06-23 15:55:28 +0430

    new version

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-      filename: "bundle.[hash].js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    // resolve: {
-    //   modules: [__dirname, "src", "node_modules"],
-    //   extensions: [".js", ".jsx", ".tsx", ".ts"],
-    // },
-    mode: 'development',//'production',
-    // entry: './src/Highlight.js',
+    // mode: 'development',
+    // entry: "./src/index.js",
     // output: {
-    //     path: path.resolve('lib'),
-    //     filename: 'Highlight.js',
-    //     libraryTarget: 'commonjs2',
+    //   filename: "bundle.[hash].js",
+    //   path: path.resolve(__dirname, "dist"),
     // },
+    mode:'production',
+    entry: './src/Highlight.js',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'Highlight.js',
+        libraryTarget: 'commonjs2',
+    },
     module: {
         rules: [
             {
@@ -39,25 +36,25 @@ module.exports = {
         template: "./src/index.html",
       }),
     ],
-    // resolve: {
-    //     alias: {
-    //         'react': path.resolve(__dirname, './node_modules/react'),
-    //         'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    //     }
-    // },
-    // externals: {
-    //     // Don't bundle react or react-dom
-    //     react: {
-    //         commonjs: "react",
-    //         commonjs2: "react",
-    //         amd: "React",
-    //         root: "React"
-    //     },
-    //     "react-dom": {
-    //         commonjs: "react-dom",
-    //         commonjs2: "react-dom",
-    //         amd: "ReactDOM",
-    //         root: "ReactDOM"
-    //     }
-    // }
+    resolve: {
+        alias: {
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+        }
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        }
+    }
 };
